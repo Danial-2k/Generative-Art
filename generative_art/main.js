73e3552175cae71092f1bdc4939b424ec0981a6c
Danial-2k <134682633+Danial-2k@users.noreply.github.com>
2023-05-26 12:07:52 +0200

    Add files via upload

---
+++ generative_art/main.js
@@ -0,0 +1,99 @@
+// Generative Art - Übung: Danial
+// Tutorial-Quelle: https://youtu.be/Yvz_axxWG4Y
+
+const canvas = document.getElementById('canvas_1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+const mouse = {
+    x: undefined,
+    y: undefined, // canvas ist leer bevor wir darüber hovern
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for (let i = 0; i < 10; i++){
+        particlesArray.push(new Particle());
+    }
+});
+
+canvas.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for (let i = 0; i < 5; i++){ //Anzahl Partikel
+        particlesArray.push(new Particle());
+    }
+})
+
+class Particle {
+    constructor(){ // wird bei jeder JavaScript Class benötigt
+        this.x = mouse.x;
+        this.y = mouse.y;
+        //this.x = Math.random() * canvas.width;
+        //this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 15 + 1; // zufällige Größe zwischen 1 und 6
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5; // positive und negative Werte, damit sich Partikel nicht nur in eine Richtung bewegen
+        this.color = 'hsl(' + hue + ', 100%, 50%)';
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1; // Kreise schrumpfen 
+    }
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function handleParticles(){
+    for (let i = 0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+
+        for (let j = i; j < particlesArray.length; j++){
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y; // siehe Satz des Pythagoras
+            const distance = Math.sqrt(dx * dx + dy * dy); // Kalkuliert Distanz zwischen allen Partikeln
+            if (distance < 100){
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color; // färbt Striche
+                ctx.lineWidth = 0.3; // Strichstärke
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke(); // Striche werden erzeugt
+                ctx.closePath();
+            }
+        }
+        if (particlesArray[i].size <= 0.3){
+            particlesArray.splice(i, 1);
+            console.log(particlesArray.length);
+            i--;
+        }
+    }
+}
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // entfernt alte Farbspur (Streifen)
+    //ctx.fillStyle = 'rgba(0, 0, 0, 0.04)';
+    //ctx.fillRect(0, 0,  canvas.width, canvas.height);
+    handleParticles();
+    hue+=2; // Geschwindigkeit des Durchgangs durch Farbspektrum
+    requestAnimationFrame(animate);
+}
+animate();
+
+
+
